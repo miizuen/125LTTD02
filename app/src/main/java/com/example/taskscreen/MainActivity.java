5db7883988b140171e0cd4c1dd423fbd15f9b314
Gnourt108 <23115053122146@sv.ute.udn.vn>
2025-09-24 09:39:43 +0700

    Fourth commit - add event in MainActivity.java

--- app/src/main/java/com/example/taskscreen/MainActivity.java
+++ app/src/main/java/com/example/taskscreen/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.taskscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnScreenA, btnScreenB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnScreenA = findViewById(R.id.btnScreen1);
+        btnScreenB = findViewById(R.id.btnScreen2);
+
+        btnScreenA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScreenA.class);
+                startActivity(intent);
+            }
+        });
+
+        btnScreenB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScreenB.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
